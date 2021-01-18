9f871de2b7a0106b244c23409446710fae9b6c37
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-01-18 17:21:17 -0500

    function createFunctionPrinter solution

--- closure-challenges/2-createFunctionPrinter.js
+++ closure-challenges/2-createFunctionPrinter.js
@@ -4,10 +4,26 @@
 // CHALLENGE 2
 function createFunctionPrinter(input) {
 
+    function innerFunc(){
+        return console.log(input)
+
+    return innerFunc
 }
 
 // /*** Uncomment these to check your work! ***/
-// const printSample = createFunctionPrinter('sample');
-// printSample(); // => should console.log('sample');
-// const printHello = createFunctionPrinter('hello');
-// printHello(); // => should console.log('hello');
+const printSample = createFunctionPrinter('sample');
+printSample(); // => should console.log('sample');
+
+const printHello = createFunctionPrinter('hello');
+printHello(); // => should console.log('hello');
+
+
+/*
+Line 5: declare function createFunctionPrinter in global memory-- taking input as a parameter 
+Line 14: declaring constant printSample
+printSample is equal to the result of calling createFunctionPrinter('sample');
+printSample is initially undefined, but createFunctionPrinter returns the function definition of innerFunc
+line 15: we are invoking printSample(); -- we know printSample is a label for innerFunc 
+we get the string "sample" returned
+
+*/
