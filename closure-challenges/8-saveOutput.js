5f20895fd404d96d059f1987feff8350b7b4e13c
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-01-18 18:39:34 -0500

    saveOutput solution

--- closure-challenges/8-saveOutput.js
+++ closure-challenges/8-saveOutput.js
@@ -3,13 +3,26 @@
 
 
 // CHALLENGE 8
-function saveOutput(func, magicWord) {
+function saveOutput(callback, magicWord) {
+    let resultObj = {}
 
+    function innerFunc(arg){
+        if (arg === magicWord){
+            return resultObj
+        } else {
+            let result = callback(arg)
+            resultObj[arg] = result
+            return result 
+        }
+    }
+    return innerFunc
 }
 
 // /*** Uncomment these to check your work! ***/
-// const multiplyBy2 = function(num) { return num * 2; };
-// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-// console.log(multBy2AndLog(2)); // => should log 4
-// console.log(multBy2AndLog(9)); // => should log 18
-// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+const multiplyBy2 = function(num) { return num * 2; };
+
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+
+console.log(multBy2AndLog(2)); // => should log 4
+console.log(multBy2AndLog(9)); // => should log 18
+console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
