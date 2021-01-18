04d2316a1946969ba44d22a2a457576fe205b650
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-01-18 17:49:17 -0500

    once solution

--- closure-challenges/4-once.js
+++ closure-challenges/4-once.js
@@ -2,13 +2,30 @@
 // Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time, it should call the callback and return that output. If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
 
 // CHALLENGE 4
-function once(func) {
+function once(callback) {
+    let count = 0;
+    let result;
 
+    function innerFunc(num){
+        if(count === 0){
+            result = callback(num); // 6
+            count++ // count = 1
+            return result // 6
+        } else {
+            return result //6
+        }
+        
+    }
+   return innerFunc
 }
 
 // /*** Uncomment these to check your work! ***/
-// const onceFunc = once(addByTwo);
-// console.log(onceFunc(4));  // => should log 6
-// console.log(onceFunc(10));  // => should log 6
-// console.log(onceFunc(9001));  // => should log 6
+let addByTwo = function(num){
+    return num + 2
+}
+const onceFunc = once(addByTwo);
+
+console.log(onceFunc(4));  // => should log 6
+console.log(onceFunc(10));  // => should log 6
+console.log(onceFunc(9001));  // => should log 6
 
