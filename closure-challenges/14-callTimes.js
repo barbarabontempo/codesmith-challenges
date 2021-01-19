1be80d7d1daaa5607a3ac04faab78bf382ae1abe
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-01-18 19:51:11 -0500

    solution to question 14

--- closure-challenges/14-callTimes.js
+++ closure-challenges/14-callTimes.js
@@ -2,13 +2,22 @@
 // Write a function, callTimes, that returns a new function. The new function should return the number of times it’s been called.
 
 function callTimes() {
+let count = 1;
 
+    function innerFunc(){
+        let times = count
+        count++ 
+        return times 
+    }
+
+    return innerFunc;
 }
 
 // /*** Uncomment these to check your work! ***/
 let myNewFunc1 = callTimes();
 let myNewFunc2 = callTimes();
-myNewFunc1(); // => 1
-myNewFunc1(); // => 2
-myNewFunc2(); // => 1
-myNewFunc2(); // => 2
+
+console.log(myNewFunc1()); // => 1
+console.log(myNewFunc1()); // => 2
+console.log(myNewFunc2()); // => 1
+console.log(myNewFunc2()); // => 2
