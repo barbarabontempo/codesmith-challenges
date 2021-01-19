7ec14eab3d5a6b0f2a16c5cde1514cf25e94acae
kceron <karemiceron@gmail.com>
2021-01-19 08:43:23 -0500

    average

--- closure-challenges/15-russianRoulette.js
+++ closure-challenges/15-russianRoulette.js
@@ -2,13 +2,24 @@
 // Create a function russianRoulette that accepts a number (let us call it n), and returns a function. The returned function will take no arguments, and will return the string 'click' the first n - 1 number of times it is invoked. On the very next invocation (the nth invocation), the returned function will return the string 'bang'. On every invocation after that, the returned function returns the string 'reload to play again'.
 
 function russianRoulette(num) {
-
+  let count = 0
+  function innerFunc(){
+    count++
+    if(count < num){
+      return "click"
+    }else if(count === num){
+      return"bang"
+    }else{
+      return 'reload to play again'
+    }
+  }
+  return innerFunc
 }
 
 // /*** Uncomment these to check your work! ***/
-// const play = russianRoulette(3);
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'bang'
-// console.log(play()); // => should log 'reload to play again'
-// console.log(play()); // => should log 'reload to play again'
+const play = russianRoulette(3);
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'bang'
+console.log(play()); // => should log 'reload to play again'
+console.log(play()); // => should log 'reload to play again'
--- closure-challenges/16-average.js
+++ closure-challenges/16-average.js
@@ -1,17 +1,27 @@
 // Challenge 16
 // Create a function average that accepts no arguments, and returns a function (that will accept either a number as its lone argument, or no arguments at all). When the returned function is invoked with a number, the output should be average of all the numbers have ever been passed into that function (duplicate numbers count just like any other number). When the returned function is invoked with no arguments, the current average is outputted. If the returned function is invoked with no arguments before any numbers are passed in, then it should return 0.
 
-
 // CHALLENGE 16
 function average() {
-
-}
+  let average = 0
+  let counter = 0
+  function innerFunc(num){
+    if(num){
+      counter++
+      average += num
+      return average/counter
+    }else{
+      return counter === 0 ? 0 : average/counter
+    }
+  };
+  return innerFunc
+};
 
 // /*** Uncomment these to check your work! ***/
-// const avgSoFar = average();
-// console.log(avgSoFar()); // => should log 0
-// console.log(avgSoFar(4)); // => should log 4
-// console.log(avgSoFar(8)); // => should log 6
-// console.log(avgSoFar()); // => should log 6
-// console.log(avgSoFar(12)); // => should log 8
-// console.log(avgSoFar()); // => should log 8
+const avgSoFar = average();
+console.log(avgSoFar()); // => should log 0
+console.log(avgSoFar(4)); // => should log 4
+console.log(avgSoFar(8)); // => should log 6
+console.log(avgSoFar()); // => should log 6
+console.log(avgSoFar(12)); // => should log 8
+console.log(avgSoFar()); // => should log 8
