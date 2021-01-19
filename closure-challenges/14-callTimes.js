d462c5eccc7d9d8805772583f8451effe0e45c08
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-01-18 19:45:35 -0500

    fixed question 14

--- closure-challenges/14-callTimes.js
+++ closure-challenges/14-callTimes.js
@@ -1,4 +1,4 @@
-// Challenge 13
+// Challenge 14
 // There's no such thing as private properties on a JavaScript object! But, maybe there are? Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods. getSecret() which returns the secret setSecret() which sets the secret
 
 function callTimes() {
@@ -6,9 +6,9 @@ function callTimes() {
 }
 
 // /*** Uncomment these to check your work! ***/
-// let myNewFunc1 = callTimes();
-// let myNewFunc2 = callTimes();
-// myNewFunc1(); // => 1
-// myNewFunc1(); // => 2
-// myNewFunc2(); // => 1
-// myNewFunc2(); // => 2
+let myNewFunc1 = callTimes();
+let myNewFunc2 = callTimes();
+myNewFunc1(); // => 1
+myNewFunc1(); // => 2
+myNewFunc2(); // => 1
+myNewFunc2(); // => 2
