e0128ad35dbf0540734eb8856a9889ae9a95c073
kceron <karemiceron@gmail.com>
2021-01-18 19:05:14 -0500

    dateStamp

--- closure-challenges/11-dateStamp.js
+++ closure-challenges/11-dateStamp.js
@@ -3,12 +3,20 @@
 
 
 // CHALLENGE 11
-function dateStamp(func) {
-
+function dateStamp(callback) {
+  let date = new Date().toLocaleDateString()
+  let obj = {}
+  function innerFunc(num){
+    let result = callback(num)
+    obj["date"] = date
+    obj["output"] = result
+    return obj
+  }
+  return innerFunc
 }
 
 // /*** Uncomment these to check your work! ***/
-// const stampedMultBy2 = dateStamp(n => n * 2);
-// console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
-// console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
 
